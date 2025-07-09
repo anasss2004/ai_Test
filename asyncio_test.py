8b042a81ffedef8c0e50d5d6a9c09d7b9cd73ce0
anasss2004 <127718807+anasss2004@users.noreply.github.com>
2025-07-09 16:04:27 +0300

    Update asyncio_test.py

--- asyncio_test.py
+++ asyncio_test.py
@@ -1,9 +1,9 @@
 import asyncio
 import time
 import aiofiles
-chunksize=1024 * 1024
+chunk_size=1024 * 1024
 li=0
-async def READ(text):
+async def read(text):
      #not error handling
      global li
      async with aiofiles.open(text, 'r') as file:
@@ -11,7 +11,8 @@ async def READ(text):
             li+=1
             async with aiofiles.open(f'processed-output asyncio/{li}processed.txt',"w") as f:
                 await f.write(chunk.upper())
-print("processing")
-start=time.perf_counter()
-asyncio.run(READ('text_1.txt'))
-print(f"this took {time.perf_counter()-start}")
+if __name__=='__main__':
+     print("processing")
+     start=time.perf_counter()
+     asyncio.run(READ('text_1.txt'))
+     print(f"this took {time.perf_counter()-start}")
