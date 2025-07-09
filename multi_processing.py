57d091e1442de16bec0571003fd961695fa15015
anasss2004 <127718807+anasss2004@users.noreply.github.com>
2025-07-09 17:39:21 +0300

    Update multi_processing.py

--- multi_processing.py
+++ multi_processing.py
@@ -1,33 +1,31 @@
 import multiprocessing
 import time
-li=0
-list=[]
+import os
 chunk_size=1024 * 1024
 def read(text):
      #not error handling
+     chunkz=[]
      with open(text, 'r') as file:
         while chunk:= file.read(chunk_size):
-            list.append(chunk)
-def capitale(text):
-    global li
-    li+=1
+            chunkz.append(chunk)
+     return chunkz          
+def capitale(args):
+    index,text=args
     uppertext=text.upper()
-    with open(f'processed-output multi processing/{li}processed.txt',"w") as f:
+    os.makedirs('processed-output multi processing', exist_ok=True)
+    with open(f'processed-output multi processing/{index+1}processed.txt',"w") as f:
         f.write(uppertext)
 
 def multiprocess_file(text,num):
-    read(text)
+    chunkz=read(text)
     for i in range(num):
         print(f"processing with {i+1} processes")
         start=time.perf_counter()
         # Create a pool of workers to handle the file download tasks
         with multiprocessing.Pool(processes=i+1) as pool:
         # Map the download tasks to the pool of worker processes
-            pool.map(Capitale,list)
-            print(f"this took{time.perf_counter()-start}")
-        
-            
-            
+            pool.map(capitale,[(index, chunk) for index, chunk in enumerate(chunkz)])
+            print(f"this took{time.perf_counter()-start:.2f}")
 if __name__=='__main__':
     multiprocess_file("text_1.txt",4)
         
