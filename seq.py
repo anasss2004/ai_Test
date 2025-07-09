b08816462b11d6f4aca005d2c53d025503e3dfe3
ans <anas.elkot2004@gmail.com>
2025-07-09 19:29:34 +0300

    clearin multi processing folder

--- seq.py
+++ seq.py
@@ -4,10 +4,11 @@ import time
 def Capital(text):
      #not error handling
      with open(text, 'r') as file:
-        content = file.read().upper()
+        content=file.read().upper()
+        print(content)
 #execute
 print("Starting processing text...")
 start= time.perf_counter()
 Capital("text_1.txt")
 print("ALL CAPITAL NOW")
-print(f"Total time taken: {time.perf_counter() - start}")
+print(f"Total time taken: {time.perf_counter() - start:.2f}")
