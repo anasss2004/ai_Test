d692159bfc11fead8cd140060964de7367a6833a
anasss2004 <127718807+anasss2004@users.noreply.github.com>
2025-07-09 16:09:25 +0300

    Update multi threading.py

--- multi threading.py
+++ multi threading.py
@@ -2,20 +2,20 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 li=0
 list=[]
-chunksize=1024 * 1024
-def READ(text):
+chunk_size=1024 * 1024
+def read(text):
      #not error handling
      with open(text, 'r') as file:
-        while chunk:= file.read(chunksize):
+        while chunk:= file.read(chunk_size):
             list.append(chunk)
-def Capitale(text):
+def capitale(text):
     global li
     li+=1
     uppertext=text.upper()
     with open(f'processed-output multi threading/{li}processed.txt',"w") as f:
         f.write(uppertext)
 def multithread_file(text,num):
-    READ(text)
+    read(text)
     for i in range(num):
         print(f"processing with {i+1} threads")
         start=time.perf_counter()
